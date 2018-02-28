ed836101c58db69d2857f52cd9197c200240bdfe
mikee1594 <mikeematos1594@gmail.com>
2018-02-28 14:31:19 +0000

    fodace esta merda

--- csharplesson1/Program.cs
+++ csharplesson1/Program.cs
@@ -4,12 +4,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace csharplesson1
+namespace csharplesson1 //thing that groups together classes 
 {
-    class Program
+    class Program // groups methods and other things
     {
-        static void Main(string[] args)
+        static void Main(string[] args)  //method = ? .... group together statements and expressions ( things with "...")
         {
+            Console.WriteLine("Hello World"); //statement 1  (change state of smth)    + ("1+1" = statement or string( idc if has or hasnt numeric || 1+1 = numeric))
+            Console.ReadKey();                  // statement 2 /change state of smth2, stop execution until u press enter
+
         }
     }
 }
